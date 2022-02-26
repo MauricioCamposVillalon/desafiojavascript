02172c046269900311c08836ac715c74829e0d2b
Mauricio Campos <thoyferr@gmail.com>
2022-02-26 11:51:49 -0300

    Ejercicio 1 listo

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -3,19 +3,16 @@ let formula = document.getElementById("formulario");
 formula.addEventListener("submit", function (event) {
    event.preventDefault();
    limpiarErrores();
-  // document.querySelector(".errorNombre").innerHTML = "Error ";
-  let txtnombre = document.getElementById('nombre').value;
-  let txtasunto = document.getElementById('asunto').value;
-  let txtmensaje = document.getElementById('mensaje').value;
+   let txtnombre = document.getElementById('nombre').value;
+   let txtasunto = document.getElementById('asunto').value;
+   let txtmensaje = document.getElementById('mensaje').value;
 
-  console.log(txtnombre);
-  let res = validar(txtnombre,txtasunto,txtmensaje);
-   if(res === true) {
-      exito();
-      };
+   let res = validar(txtnombre, txtasunto, txtmensaje);
+   if (res === true) {
+      smsPaso();
+   };
 
 });
-
 
 function limpiarErrores() {
    document.querySelector(".errorNombre").innerHTML = "";
@@ -23,28 +20,27 @@ function limpiarErrores() {
    document.querySelector(".errorMensaje").innerHTML = "";
 };
 
-function exito() {
-   document.querySelector(".resultado").innerHTML = "Formulario pasó la    validación";
+
+
+function validar(nombre, asunto, mensaje) {
+   let estado = true;
+   let validacion = /[a-zA-Z]/gim;
+   if (validacion.test(nombre) === false) {
+      document.querySelector(".errorNombre").innerHTML = "Nombre es Requerido."
+      estado = false;
+   };
+   if (validacion.test(asunto) === false) {
+      document.querySelector(".errorAsunto").innerHTML = "Asunto es requerido."
+      estado = false;
+   };
+   if (validacion.test(mensaje) === false) {
+      document.querySelector(".errorMensaje").innerHTML = "Mensaje es requerido"
+      estado = false;
+   };
+   return estado;
 };
 
 
-function validar(nombre,asunto,mensaje) {
-   let estado = true;
-   let validacion = /[a-zA-Z]/gim;
-   if (validacion.test(nombre) == false) {
-   document.querySelector(".errorNombre").innerHTML = "Nombre es Requerido."
-   estado = false;
-   };
-   if (validacion.test(asunto) == false) {
-   document.querySelector(".errorAsunto").innerHTML = "Asunto es requerido."
-   estado = false;
-   };
-   if (validacion.test(mensaje) == false ) {
-   document.querySelector(".errorMensaje").innerHTML = "Mensaje es requerido"
-   estado = false;
-   };
-   return estado;
-   };
-
-
-
+function smsPaso() {
+   document.querySelector(".resultado").innerHTML = "Mensaje enviado con éxito !!!";
+};
